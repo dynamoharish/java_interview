ba28fd849f916b4336285042d1d1ea9d691988e9
dynamoharish <harishkannan527@gmail.com>
2023-01-06 18:54:51 +0000

    swap2numbers all logic

--- swap2numbers.java
+++ swap2numbers.java
@@ -4,16 +4,51 @@ class swap2numbers{
 
 	public static void main(String args[]){
 
-	int a,b;
+	int a,b,c,t;
 	Scanner sc=new Scanner(System.in);
 	System.out.println("Enter the A value: ");
 	a=sc.nextInt();
 	System.out.println("Enter the B value: ");
 	b=sc.nextInt();
 	
+/*	//logic 1:-
+	
+	t=a;
+	a=b;
+	b=t;
+
+	System.out.println("After Swapping A value is "+a);
+	System.out.println("After swapping B value is "+b);
+
+	//logic 2:-	
 	a=a+b;
 	b=a-b;
 	a=a-b;
+
+	System.out.println("After Swapping A value is "+a);
+	System.out.println("After swapping B value is "+b);
+
+	//logic 3:-
+	a=a*b;
+	b=a/b;
+	a=a/b;
+
+	System.out.println("After Swapping A value is "+a);
+	System.out.println("After swapping B value is "+b);
+
+	//logic 4:-
+	a=a^b;
+	b=a^b;
+	a=a^b;
+
+	System.out.println("After Swapping A value is "+a);
+	System.out.println("After swapping B value is "+b);
+	
+*/
+	//logic 5;
+
+	b=a+b-(a=b);
+	
 	System.out.println("After Swapping A value is "+a);
 	System.out.println("After swapping B value is "+b);
 
