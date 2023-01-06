402aec5e44a5d65bc4254c626f77bb69fbe093c6
dynamoharish <harishkannan527@gmail.com>
2023-01-06 22:05:32 +0000

    all logic updated

--- reverse_number.java
+++ reverse_number.java
@@ -10,6 +10,8 @@ System.out.println("Enter the any Number");
 
 int num=sc.nextInt();
 
+/*
+//Logic 1(Using Algorithm)
 int rev=0;
 
 while(num!=0){
@@ -18,6 +20,24 @@ while(num!=0){
 	num=num/10;
 }
 System.out.println("Reversed Number is "+rev);
+
+//Logic 2(Using StringBuffer class)
+
+StringBuffer rev;
+StringBuffer sb=new StringBuffer(String.valueOf(num));
+rev = sb.reverse();
+System.out.println("Reversed Value is "+rev);
+
+*/
+
+//Logic 3("Using StringBuilder class")
+
+StringBuilder sb=new StringBuilder();
+sb.append(num);
+StringBuilder rev=sb.reverse();
+System.out.println(rev);
+
+
 
 }
 
