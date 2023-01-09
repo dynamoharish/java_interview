6fd6be4ffe6f702091567a1f0633c8ca269430ab
dynamoharish <96344652+dynamoharish@users.noreply.github.com>
2023-01-09 10:44:58 +0600

    Fibonaccii without using Recursion

---
+++ fibanocci.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+class fibanocci{
+public static void main(String args[])
+{
+ int n1=0,n2=1,n3,i,count=10;
+
+ Scanner sc=new Scanner(System.in);
+
+ System.out.println("Enter the Count: ");
+ count = sc.nextInt();
+
+ //Without Using Recursion
+ System.out.print(n1+" "+n2);
+
+
+ for(i=2;i<count;i++)
+ {
+  n3=n1+n2;
+  System.out.print(" "+n3);
+  n1=n2;
+  n2=n3;
+ }
+
+}}
