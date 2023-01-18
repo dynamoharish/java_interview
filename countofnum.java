40365e25d0ac5676caf294fdf771f1e87b2ac563
dynamoharish <harishkannan527@gmail.com>
2023-01-18 17:50:59 +0000

    user input is updated

--- countofnum.java
+++ countofnum.java
@@ -1,12 +1,15 @@
+import java.util.Scanner;
 class countofnum{
 public static void main(String args[]){
-
-int number=1234637;
+Scanner sc=new Scanner(System.in);
+int number;
+System.out.println("Enter the Number: ");
+number = sc.nextInt();
 int count=0;
 while(number>0){
 number=number/10;
 count++;
 }
-System.out.println("No of digits is "+count);
+System.out.println("No of digits in Number is: "+count);
 
 }}
