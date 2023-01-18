60997a4e918302358e0d7de067ff5ffa92203a9d
dynamoharish <harishkannan527@gmail.com>
2023-01-18 17:53:52 +0000

    user input is updated in oddeven

--- oddevencount.java
+++ oddevencount.java
@@ -1,7 +1,9 @@
+import java.util.Scanner;
 class oddevencount{
 public static void main(String args[]){
-
-int number=1266613;
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter the Number: ");
+int number = sc.nextInt();
 int odd_count=0;
 int even_count=0;
 int newnumber;
@@ -19,8 +21,8 @@ odd_count++;
 }
 number=number/10;
 }
-System.out.println("No of Odd number is"+odd_count);
-System.out.println("No of Even number is"+even_count);
+System.out.println("No of Odd number is: "+odd_count);
+System.out.println("No of Even number is: "+even_count);
 
 }
 }
