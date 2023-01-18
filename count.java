0486d91009b511110b6795afc3f1ac052ffc29ed
dynamoharish <harishkannan527@gmail.com>
2023-01-18 07:16:57 +0000

    count no of digits

---
+++ count.java
@@ -0,0 +1,12 @@
+class count{
+public static void main(String args[]){
+
+int number=1234637;
+int count=0;
+while(number>0){
+number=number/10;
+count++;
+}
+System.out.println("No of digits is "+count);
+
+}}
